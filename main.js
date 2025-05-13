3dd50159e1d2f573363561c55b59485fea006ea8
Erremme <Erremme2.0@gmail.com>
2025-05-13 08:48:51 +0200

    feat: data recovery with promise.all()

--- main.js
+++ main.js
@@ -18,3 +18,45 @@ Restituire una Promise che risolve un oggetto contenente i dati aggregati.
 Stampare i dati in console in un messaggio ben formattato.
 Testa la funzione con la query "london"
 */
+
+async function fetchJson(url) {
+    const response = await fetch(url);
+    const obj = await response.json();
+    return obj;
+}
+
+async function getDashboardData(query) {
+    const destinationPromise = fetchJson(`http://localhost:5000/destinations?search=${query}`)
+    const weatherPromise = fetchJson(`http://localhost:5000/weathers?search=${query}`)
+    const airportPromise = fetchJson(`http://localhost:5000/airports?search=${query}`)
+
+    const promises = [destinationPromise, weatherPromise, airportPromise];
+    const [destinations ,weather, airport  ] = await Promise.all(promises)
+
+    return {
+        city: destinations[0].name,
+        country: destinations[0].country,
+        temperature: weather[0].temperature,
+        weather: weather[0].weather_description,
+        airport: airport[0].name
+    }
+
+  
+}
+
+getDashboardData('london')
+    .then(data => {
+        console.log('Dasboard data:', data);
+        console.log(
+            `${data.city} is in ${data.country}.\n` +
+            `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`+
+            `The main airport is ${data.airport}.\n`
+        );
+    })
+    .catch(error => console.error(error));
+
+
+
+
+
+
